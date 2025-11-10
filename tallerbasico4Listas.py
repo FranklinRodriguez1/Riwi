3d8a2373f377c6fc9a7f33d7bdf50dc25b5845e5
FERC <frro2121.m@gmail.com>
2025-11-10 13:08:30 -0500

    agrego taller basico 4 aun por terminar

---
+++ tallerbasico4Listas.py
@@ -0,0 +1,20 @@
+# Objetivo: Crear, recorrer, modificar y eliminar elementos en listas.
+
+#     Lista de frutas. 
+frutas = ["manzana", "banana", "cereza"]
+print("Frutas iniciales:", frutas)
+#     Agregar y eliminar frutas. 
+frutas.append(input("Ingresa una fruta para agregar: "))
+#     Buscar un elemento en la lista. 
+frutaABuscar = input("ingresa una fruta para buscar=> ")
+#     Lista de números y promedio.  
+numeros = [10, 20, 30, 40, 50] 
+promedio  = sum(numeros)/len(numeros)
+print(f"el promedio de {numeros} es: {promedio}")
+#     Números pares: guardar solo los pares. 
+numeros_pares = [] 
+for i in range(1, 21):
+    if i % 2 == 0:
+        numeros_pares.append(i)
+#     Eliminar duplicados.
+listaConDuplicados = [1, 2, 2, 3, 4, 4, 5]
