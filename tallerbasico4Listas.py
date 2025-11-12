5b33f3fe20b615934375d98752e89aebc8347685
frank <frro2121.m@gmail.com>
2025-11-12 13:49:24 -0500

    se agrega funcionalidad completa programa de frutas

--- tallerbasico4Listas.py
+++ tallerbasico4Listas.py
@@ -1,29 +1,48 @@
 # Objetivo: Crear, recorrer, modificar y eliminar elementos en listas.
-
-#     Lista de frutas. 
 frutas = ["manzana", "banana", "cereza"]
-print("Frutas iniciales:", frutas)
-#     Agregar y eliminar frutas. 
-frutas.append(input("Ingresa una fruta para agregar: "))  
-print(f"estos son las frutas en el inventario{frutas}") 
-frutaDelete = input("ingresa una fruta que quieras eliminar de lo contrario escribe x")
 
-#     Buscar un elemento en la lista. 
-frutaABuscar = input("ingresa una fruta para buscar=> ") 
-for i in frutas: 
-    if i == frutaABuscar: 
-        print(f"el producto {i} ya se encuentra registrado") 
-        
-     
+while True:    
+    # Lista de frutas.  
+    option = input("¿Que Deseas Hacer?\n"
+    "1 . Ver listado de frutas \n" 
+    "2 . Agregar frutas\n" 
+    "3 . Eliminar frutas \n" 
+    "4 . Buscar una fruta dentro de la lista\n" 
+    "5 . Salir \n") 
+    #mostrar frutas 
+    if int(option) == 1:
+        print(frutas)  
+    # Agregar frutas
+    if int(option) == 2:
+        frutas.append(input("Ingresa una fruta para agregar: "))   
+        print("fruta Agregada") 
+        for i in frutas:
+            print(i)   
+    # eliminar frutas. 
+    if int(option) == 3:
+        frutaDelete = input("ingresa una fruta que quieras eliminar de lo contrario escribe x") 
+        for i in frutas: 
+            if frutaDelete == i:
+                frutas.remove(i)  
+                print("Elemento eliminado") 
+                print(f"objetos en inventario {i}")
+    #     # Buscar un elemento en la lista. 
+    if int(option) == 4:
+        frutaABuscar = input("ingresa una fruta para buscar=> ") 
+        for i in frutas: 
+            if i == frutaABuscar: 
+                print(f"el producto {i} ya se encuentra registrado")  
+    if int(option) == 5: 
+        print("¡¡Que tengas feliz dia!!")
+        break
 
-# ____________________________________________
-#     Lista de números y promedio.
-numeros = [10, 20, 30, 40, 50] 
-promedio  = sum(numeros)/len(numeros)
-print(f"el promedio de {numeros} es: {promedio}") 
+# Lista de números y promedio.
+# numeros = [10, 20, 30, 40, 50] 
+# promedio  = sum(numeros)/len(numeros)
+# print(f"el promedio de {numeros} es: {promedio}") 
 
 # #     Números pares: guardar solo los pares.  
-# def imparImpar (): #FUNCION PARA DEVOLVER PARES E IMPARES
+# def imparImpar i(): #FUNCION PARA DEVOLVER PARES E IMPARES
 #     numbers = [73, 24, 67, 52, 19, 88, 45, 90, 33, 12] 
 #     pares = [] 
 #     impares =[]
@@ -37,8 +56,8 @@ print(f"el promedio de {numeros} es: {promedio}")
 # #_____________________________________________________
 # for i in range(1, 21):
 #     if i % 2 == 0:
-#         numeros_pares.append(i)
-#     Eliminar duplicados.
+#         numerospares.append(i)
+#     # Eliminar duplicados.
 # listaConDuplicados = [5, 6, 70, 5, 3, 1, 2, 8, 9, 9, 9, 8, ] 
 # sinDuplicados =list(set(listaConDuplicados))
 # ordenado = sorted(sinDuplicados, reverse=True)
